4db58c5ae62a5d790de2c5faec9ecf6a8b697e82
davidnbresch <david.bresch@gmail.com>
2016-05-29 14:44:10 +0200

    eq global hazard using parfor
    substantial further speedup. Old version (non-parallel) keep as _slow

--- code/eq_global_hazard_set_slow.m
+++ code/eq_global_hazard_set_slow.m
@@ -1,10 +1,12 @@
-function hazard=eq_global_hazard_set(eq_data,hazard_set_file,centroids,TEST_epicenter_preselection,correction,a1,a2,a3,a4,b)
+function hazard=eq_global_hazard_set_slow(eq_data,hazard_set_file,centroids,TEST_epicenter_preselection,correction,a1,a2,a3,a4,b)
 % Create earthquake hazard set
 % MODULE
 % eq_global
 % NAME:
 %   eq_global_hazard_set
 % PURPOSE:
+%   SLOW version of eq_global_hazard_set
+%
 %   generate an earthqake (EQ) hazard event set, starting from epicenters
 %   calculating attenuation (distance and depth) to convert Richter
 %   magnitude to a modified Mercalli intensity (MMI)
@@ -12,13 +14,9 @@ function hazard=eq_global_hazard_set(eq_data,hazard_set_file,centroids,TEST_epic
 %   previous step: see eq_global_probabilistic or eq_isc_gem_read (or
 %   eq_centennial_read, or eq_signieq_read)
 % CALLING SEQUENCE:
-%   
-%   hazard=eq_global_hazard_set(eq_data,hazard_set_file,centroids)
+%   hazard=eq_global_hazard_set_slow(eq_data,hazard_set_file,centroids)
 % EXAMPLE:
-%   eq_data=eq_global_probabilistic(eq_isc_gem_read); % global
-%   centroids=climada_centroids_load('USFL_MiamiDadeBrowardPalmBeach');
-%   hazard=eq_global_hazard_set(eq_data,'_EQ_TEST',centroids);
-%   %hazard=eq_global_hazard_set(eq_global_probabilistic(eq_isc_gem_read))
+%   hazard=eq_global_hazard_set_slow(eq_global_probabilistic(eq_isc_gem_read))
 % INPUTS:
 % OPTIONAL INPUT PARAMETERS:
 %   eq_data: a structure with EQ epicenters, see eq_isc_gem_read
@@ -76,7 +74,7 @@ function hazard=eq_global_hazard_set(eq_data,hazard_set_file,centroids,TEST_epic
 % Melanie Bieli, melanie.bieli@bluewin.ch, 20141223, added correction,a1,a2,a3,a4
 % Melanie Bieli, melanie.bieli@bluewin.ch, 20150120, hazard_arr_density=0.01
 % David N. Bresch, david.bresch@gmail.com, 20150819, climada_global.centroids_dir introduced
-% David N. Bresch, david.bresch@gmail.com, 20160529, parfor speedup, factor 3-5
+% David N. Bresch, david.bresch@gmail.com, 20160529, renamed to eq_global_hazard_set_slow
 %-
 
 hazard=[]; % init
@@ -142,10 +140,6 @@ if isempty(hazard_set_file) && TEST_epicenter_preselection<2 % local GUI
     end
 end
 
-% complete path, if missing
-[fP,fN,fE]=fileparts(hazard_set_file);
-if isempty(fP),hazard_set_file=[climada_global.data_dir filesep 'hazards' filesep fN fE];end
-
 % prompt for centroids if not given
 if isempty(centroids) % local GUI
     centroids_default    = [climada_global.centroids_dir filesep '*.mat'];
@@ -175,23 +169,6 @@ if isempty(centroids) % local GUI
     end
 end
 
-if isfield(centroids,'assets')
-    % centroids contains in fact an entity
-    entity=centroids; centroids=[]; % silly switch, but fastest
-    centroids.lat =entity.assets.lat;
-    centroids.lon=entity.assets.lon;
-    centroids.centroid_ID=1:length(entity.assets.lon);
-    % treat optional fields
-    if isfield(entity.assets,'distance2coast_km'),centroids.distance2coast_km=entity.assets.distance2coast_km;end
-    if isfield(entity.assets,'elevation_m'),centroids.elevation_m=entity.assets.elevation_m;end
-    if isfield(entity.assets,'country_name'),centroids.country_name=entity.assets.country_name;end
-    if isfield(entity.assets,'admin0_name'),centroids.admin0_name=entity.assets.admin0_name;end
-    if isfield(entity.assets,'admin0_ISO3'),centroids.admin0_ISO3=entity.assets.admin0_ISO3;end
-    if isfield(entity.assets,'admin1_name'),centroids.admin1_name=entity.assets.admin1_name;end
-    if isfield(entity.assets,'admin1_code'),centroids.admin1_code=entity.assets.admin1_code;end
-    clear entity
-end
-
 if ~isstruct(centroids) % load, if filename given
     centroids_file=centroids;centroids=[];
     fprintf('centroids read from %s\n',centroids_file);
@@ -217,19 +194,16 @@ min_year   = min(eq_data.yyyy);
 max_year   = max(eq_data.yyyy);
 orig_years = max_year - min_year+1;
 
-n_events    = length(eq_data.mag);
-n_centroids = length(centroids.lon);
-
 % fill the hazard structure
 hazard.reference_year   = hazard_reference_year;
 hazard.lon              = centroids.lon;
 hazard.lat              = centroids.lat;
 hazard.centroid_ID      = centroids.centroid_ID;
-hazard.event_count      = n_events;
-hazard.event_ID         = 1:n_events;
+hazard.event_count      = length(eq_data.mag);
+hazard.event_ID         = 1:hazard.event_count;
 hazard.orig_years       = orig_years;
 hazard.orig_event_count = eq_data.n_epicenters_orig;
-hazard.orig_event_flag  = zeros(1,n_events);
+hazard.orig_event_flag  = zeros(1,hazard.event_count);
 hazard.orig_event_flag(1:hazard.orig_event_count)=1;
 hazard.yyyy             = eq_data.yyyy;
 hazard.mm               = eq_data.mm;
@@ -237,65 +211,55 @@ hazard.dd               = eq_data.dd;
 hazard.datenum          = eq_data.datenum;
 hazard.units            = 'MMI';
 
-% allocate the hazard array (sparse, to manage MEMORY)
-% fprintf('%s: spalloc(%i,%i,%i)\n',mfilename,n_events,n_centroids,...
-%     ceil(n_events*n_centroids*hazard_arr_density));
-intensity = spalloc(n_events,n_centroids,...
-     ceil(n_events*n_centroids*hazard_arr_density));
-%intensity = zeros(n_events,n_centroids);
+% allocate the hazard array (sparse, to manage memory)
+fprintf('%s: spalloc(%i,%i,%i)\n',mfilename,hazard.event_count,length(hazard.lon),...
+    ceil(hazard.event_count*length(hazard.lon)*hazard_arr_density));
+hazard.intensity = spalloc(hazard.event_count,length(hazard.lon),...
+    ceil(hazard.event_count*length(hazard.lon)*hazard_arr_density));
 
 t0       = clock;
+n_events = hazard.event_count;
 n_events_eff=sum(in_centroids_poly);
 
-% msgstr   = sprintf('processing %i (of globally %i) epicenters',n_events_eff,n_events);
-% if climada_global.waitbar
-%     fprintf('%s (updating waitbar with estimation of time remaining every 100th epicenter)\n',msgstr);
-%     h        = waitbar(0,msgstr);
-%     set(h,'Name','Hazard EQ: shaking intensity (MMI)');
-%     mod_step = 10; % first time estimate after 10 events, then every 100
-% else
-%     fprintf('%s (waitbar suppressed)\n',msgstr);
-%     mod_step=n_events+10;
-% end
+msgstr   = sprintf('processing %i (of globally %i) epicenters',n_events_eff,n_events);
+if climada_global.waitbar
+    fprintf('%s (updating waitbar with estimation of time remaining every 100th epicenter)\n',msgstr);
+    h        = waitbar(0,msgstr);
+    set(h,'Name','Hazard EQ: shaking intensity (MMI)');
+    mod_step = 10; % first time estimate after 10 events, then every 100
+else
+    fprintf('%s (waitbar suppressed)\n',msgstr);
+    mod_step=n_events+10;
+end
 
-% make explicit and direct, speeds up parfor
-eq_data_glat=eq_data.glat;
-eq_data_glon=eq_data.glon;
-eq_data_mag=eq_data.mag;
-eq_data_dep=eq_data.dep;
-
-%event_i_eff=0; % since we only process a subset, for progress display, disabled
-parfor event_i=1:n_events
+event_i_eff=0; % since we only process a subset
+for event_i=1:n_events
     
     if in_centroids_poly(event_i)
         
-        % event_i_eff=event_i_eff+1; % for progress display, disabled
+        event_i_eff=event_i_eff+1;
         
-        intensity(event_i,:)=eq_global_attenuation(eq_data_glat(event_i),...
-            eq_data_glon(event_i),eq_data_mag(event_i),...
-            centroids, 0, eq_data_dep(event_i), correction, a1,a2,a3,a4,b);
+        hazard.intensity(event_i,:)=eq_global_attenuation(eq_data.glat(event_i),...
+            eq_data.glon(event_i),eq_data.mag(event_i),...
+            centroids, 0, eq_data.dep(event_i), correction, a1,a2,a3,a4,b);
         
-        % no sequential progress reported in parfor
-        %         if mod(event_i_eff,mod_step)==0
-        %             mod_step          = 100;
-        %             t_elapsed   = etime(clock,t0)/event_i_eff;
-        %             events_remaining  = n_events_eff-event_i_eff;
-        %             t_projected_sec   = t_elapsed*events_remaining;
-        %             if t_projected_sec<60
-        %                 msgstr = sprintf('est. %3.0f sec left (%i/%i epicenters)',t_projected_sec,   event_i_eff,n_events_eff);
-        %             else
-        %                 msgstr = sprintf('est. %3.1f min left (%i/%i epicenters)',t_projected_sec/60,event_i_eff,n_events_eff);
-        %             end
-        %             waitbar(event_i_eff/n_events_eff,h,msgstr); % update waitbar
-        %         end
+        if mod(event_i_eff,mod_step)==0
+            mod_step          = 100;
+            t_elapsed   = etime(clock,t0)/event_i_eff;
+            events_remaining  = n_events_eff-event_i_eff;
+            t_projected_sec   = t_elapsed*events_remaining;
+            if t_projected_sec<60
+                msgstr = sprintf('est. %3.0f sec left (%i/%i epicenters)',t_projected_sec,   event_i_eff,n_events_eff);
+            else
+                msgstr = sprintf('est. %3.1f min left (%i/%i epicenters)',t_projected_sec/60,event_i_eff,n_events_eff);
+            end
+            waitbar(event_i_eff/n_events_eff,h,msgstr); % update waitbar
+        end
         
     end % in_centroids_poly
     
 end %event_i
-
-hazard.intensity=sparse(intensity);
-
-%if exist('h','var'),close(h);end % dispose waitbar
+if exist('h','var'),close(h);end % dispose waitbar
 
 t_elapsed = etime(clock,t0);
 msgstr    = sprintf('processing %i epicenters took %3.2f min (%3.4f sec/event)',n_events_eff,t_elapsed/60,t_elapsed/n_events_eff);
